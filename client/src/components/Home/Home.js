1875693939eb76743f0bfe34066f169a09e75902
Monalisha Panda <monalisapanda6@gmail.com>
2022-09-17 19:55:46 +0530

    Frontent Home components

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
 import Header from "../common/Header/Header";
-
+// import{Calendar} from "primereact/calendar"
+// import{Dropdown} from "primereact/dropdown"
+import DatePicker from "react-datepicker";
+import"./Home.css";
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        // this.state = {
-        //     employees: []
-        // }
+        this.state = {
+            employees: [],
+            startDate:null,
+        }
     }
 
     componentDidMount() {
@@ -16,8 +22,117 @@ class Home extends React.Component {
   render() {
         return (
             <>
-            <Header active="Home"/>
-                <div>Welcome to Home Page</div>
+            <Header/>
+                <div className="filterFormDiv col-md-12">
+                <div className="filterContainer" style={{marginRight: "990px"}}>
+                    <div className="inlineDisplay vertiAlign mr-30">
+                        <label className='heading-label'>
+                        Floor
+                        </label>
+                        <br></br>
+                        {/* <Dropdown
+                        id="floorId"
+                        value={{}}
+                        optionsLabel="floor"
+                        filterBy="floor"
+                        
+                        /> */}
+
+                    </div>
+                    <div className="inlineDisplay vertiAlign mr-15">
+                        <label className='heading-label'>
+                        Zone
+                        </label>
+                        <br></br>
+                        {/* <Dropdown
+                        id="ZoneId"
+                        value={{}}
+                        optionsLabel="floor"
+                        filterBy="floor"
+                        
+                        /> */}
+
+                    </div>
+                    <div className="inlineDisplay vertiAlign mr-15" style={{verticalAlign: "top"}}>
+                        <label className='heading-label filterLabe'>
+                        From Date
+                        </label>
+                        <br></br>
+                        <DatePicker selected={this.state.startDate} onChange={(date) => this.setState({startDate: date})} />
+
+                    </div>
+                    <div className="inlineDisplay vertiAlign mr-15" style={{verticalAlign: "top"}}>
+                        <label className='heading-label'>
+                        TO Date
+                        </label>
+                        <br></br>
+                        <DatePicker selected={this.state.startDate} onChange={(date) => this.setState({startDate: date})} />
+
+                    </div>
+                </div>
+                <div className="inlineDisplay verMiddle right-align-run col-md-3" style={{verticalAlign: "top"}}>
+                    <button
+                    id ="floorSearch"
+                    onclick={{}}
+                    classname="primarySubmitButton btn-primary wid-150"
+                    style={{color: "white",
+                        background: "black",
+                        border: "none",
+                        fontSize: "18px",
+                        fontFamily: "Credit Suisse Type",
+                        height: "60px",
+                        width: "150px",
+                        marginRight:"20px",
+                        padding:"15px",
+                        marginTop:"20px",
+                    }}
+                    >
+                   Search
+                  
+                  <img 
+                  className='btnArrowIcon'
+                 // src={arrowicon}
+                 //alt="arrowicon"
+                 />
+                    </button>
+
+                    <button
+                    id ="floorSearch"
+                    onclick={{}}
+                    classname="primarySubmitButton btn-primary wid-150 mr-30"
+                    style={{color: "white",
+                    background: "black",
+                    border: "none",
+                    fontSize: "18px",
+                    fontFamily: "Credit Suisse Type",
+                    height: "60px",
+                    width: "200px",
+                    marginRight:"20px",
+                    padding:"15px",
+                    marginTop:"20px",
+                }}
+                    >
+                          <img 
+                  className='btnArrowIcon'
+                 // src={arrowicon}
+                // alt="arrowicon"
+                 />
+                   Create Desk Allocation
+                  
+                 
+                    </button>
+                </div>
+                <div className="mt-20" style={{background:"grey",height:"30px"}}>
+                  <div className='row'>
+
+                  </div>
+
+
+                </div>
+
+
+
+                </div>
                
             </>
         );
--- client/src/components/common/Header/TopBar.js
+++ client/src/components/common/Header/TopBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./TopBar.css";
+//import "./TopBar.css";
 
 import logo from  "../../../assests/logo.svg";
 
@@ -7,17 +7,55 @@ class TopBar extends React.Component {
    
     render(){
         return(
-            <div className="">
-                <span className ="navbar-brand" style={{height:"40px !important"}}>
-                    <img
-                    className="logoCs"
-                   // src={logo}
-                    alt="Credit Suisse Logo"
-                    />
+            <div class="topNav">
+            
+            <div class="leftwrapper" style={{float:"left",marginLeft: "90px",}}>
+                <span className="title" style={{fontSize:"20px",fontFamily:"Credit-Suisse-type"}}>
+                    My Cs Space
                 </span>
-            </div>
+                </div>
+            
+                <div class="middleWrapper">
+               
+                </div>
+                <div class="rightWrapper rightAlign padRight">
+                    <div className="wrapperleft">
+                        <span className="namefont">
+                         Monalisha Panda(CDQU 21)
+                        </span>
+                    </div>
+
+                    {/* <div className="wrapperleft">
+                        <span className="namefont">
+                       Login Date:18-09-22
+                        </span>
+                    </div> */}
+                    </div>
+
+                    <div class="wrapperRight mright-15">
+                        <div className="userIcon">
+                            <img
+                            alt="User Profile"
+                            title="User"
+                            className="userprofile"
+                           // src={userIcon}
+                            />
+                        </div>
+                </div>
+            
+           
+          
+              
+           </div>
+           
+        
+          
+           
+      
         );
     }
     
 }
+
+
 export default TopBar;
